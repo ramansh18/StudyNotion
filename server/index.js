fdfbc7bf6fe256f5ab04066159559195c81c47ad
ramansh18 <123497240+ramansh18@users.noreply.github.com>
2024-11-23 10:03:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(
 	cors({
 		origin:"http://localhost:3000",
-		"https://study-notion-project-sepia.vercel.app",
+		origin:"https://study-notion-project-sepia.vercel.app",
 		credentials:true,
 	})
 )
